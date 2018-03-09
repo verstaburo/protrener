97d0705e1679c7efb5a81b6c51cd98b62846051b
Allkrain <aukra25@yandex.ru>
2018-03-09 22:24:26 +0300

    loading animation

--- app/blocks/animations/animations.js
+++ app/blocks/animations/animations.js
@@ -5,93 +5,101 @@ import anime from 'animejs';
 const $ = window.$;
 
 export default function mainPageAnimation() {
-  const animationTimeline = anime.timeline();
-  animationTimeline
-    .add({
-      targets: '.video-bg',
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 2000,
-    })
-    .add({
-      targets: '.link-list__item',
-      translateX: ['-40px', 0],
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 500,
-      offset: 3500,
-      delay(el, i, l) {
-        return i * 150;
-      },
-    })
-    .add({
-      targets: '.sport-navigation__title',
-      translateY: ['-100px', '30px', '-30px', 0],
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 500,
-      offset: 3000,
-    })
-    .add({
-      targets: '.social',
-      scale: [0, 1],
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 500,
-      offset: 500,
-      delay(el, i, l) {
-        return i * 350;
-      },
-    })
-    .add({
-      targets: '.banner',
-      rotate: ['185deg', 0],
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 500,
-      offset: 2000,
-    })
-    .add({
-      targets: '.contacts',
-      translateY: ['100%', 0],
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 2000,
-      offset: 2500,
-    })
-    .add({
-      targets: '.brushes__item_bottom',
-      translateX: ['-100%', 0],
-      easing: 'easeOutExpo',
-      duration: 2000,
-      offset: 1000,
-    })
-    .add({
-      targets: '.brushes__item_top',
-      translateX: ['100%', 0],
-      easing: 'easeOutExpo',
-      duration: 2000,
-      offset: 1000,
-    })
-    .add({
-      targets: '.header__search, .header__news',
-      translateY: ['-100%', 0],
-      opacity: [0, 1],
-      easing: 'easeOutExpo',
-      duration: 2000,
-      delay(el, i, l) {
-        return i * 250;
-      },
-      offset: 0,
-    })
-    .add({
-      targets: '.logo__image',
-      scale: [2, 1],
-      opacity: [0, 1],
-      rotate: '2turn',
-      easing: 'easeOutExpo',
-      duration: 2000,
-      offset: 500,
-    });
+  if ($('.valve').length > 0 && $('.js').length > 0 && $(window).innerWidth() >= 1024) {
+    const animationTimeline = anime.timeline();
+    animationTimeline
+      .add({
+        targets: '.valve',
+        opacity: [1, 0],
+        easing: 'easeOutExpo',
+        duration: 250,
+      })
+      .add({
+        targets: '.video-bg',
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 2000,
+      })
+      .add({
+        targets: '.link-list__item',
+        translateX: ['-40px', 0],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 500,
+        offset: 3500,
+        delay(el, i, l) {
+          return i * 150;
+        },
+      })
+      .add({
+        targets: '.sport-navigation__title',
+        translateY: ['-100px', '30px', '-30px', 0],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 500,
+        offset: 3000,
+      })
+      .add({
+        targets: '.social',
+        scale: [0, 1],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 250,
+        offset: 750,
+        delay(el, i, l) {
+          return i * 150;
+        },
+      })
+      .add({
+        targets: '.banner',
+        rotate: ['185deg', 0],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 500,
+        offset: 2000,
+      })
+      .add({
+        targets: '.contacts',
+        translateY: ['100%', 0],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 2000,
+        offset: 2500,
+      })
+      .add({
+        targets: '.brushes__item_bottom',
+        translateX: ['-100%', 0],
+        easing: 'easeOutExpo',
+        duration: 2000,
+        offset: 1000,
+      })
+      .add({
+        targets: '.brushes__item_top',
+        translateX: ['100%', 0],
+        easing: 'easeOutExpo',
+        duration: 2000,
+        offset: 1000,
+      })
+      .add({
+        targets: '.header__search, .header__news',
+        translateY: ['-150px', 0],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 1000,
+        delay(el, i, l) {
+          return i * 250;
+        },
+        offset: 250,
+      })
+      .add({
+        targets: '.logo__image',
+        scale: [2, 1],
+        opacity: [0, 1],
+        rotate: '2turn',
+        easing: 'easeOutExpo',
+        duration: 2000,
+        offset: 1000,
+      });
+  }
 }
 /* eslint-enable no-unused-vars */
