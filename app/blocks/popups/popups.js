0ff5d2ef13b69d8cde9b193bec83caa450271b2e
Allkrain <aukra25@yandex.ru>
2018-03-11 20:23:42 +0300

    fixes search close, ie11 bug animation start page, mb user page

--- app/blocks/popups/popups.js
+++ app/blocks/popups/popups.js
@@ -18,10 +18,10 @@ export default function popups() {
     afterClose: () => {
       unfreeze();
       $('.search')[0].reset();
-      $('.search-popup__result').slideUp(() => {
-        $('.search-popup__trainers').fadeOut();
-        $('.search-popup__not-found').fadeOut();
-      });
+      $('.search-popup__result').removeClass('is-visible');
+      $('.search-popup__trainers').fadeOut(250);
+      $('.search-popup__not-found').fadeOut(250);
+      $('.search-popup__form').removeClass('is-moved');
     },
     btnTpl: {
       smallBtn: '<button data-fancybox-close class="fancybox-close-small" title="{{CLOSE}}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.41 17.41"><path d="M8.71,7.29,16,0l1.41,1.41-7.29,7.3L17.41,16,16,17.41,8.71,10.12l-7.3,7.29L0,16,7.29,8.71,0,1.41,1.41,0Z"/></svg></button>',
